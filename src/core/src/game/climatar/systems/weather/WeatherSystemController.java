0fe149b2077bf0f3547acff0ba95f458cfda0967
Wenbin Yuan <leoyuanwb@gmail.com>
2017-04-02 17:58:52 -0400

    Weather controller update

--- src/core/src/game/climatar/systems/weather/WeatherSystemController.java
+++ src/core/src/game/climatar/systems/weather/WeatherSystemController.java
@@ -1,5 +1,35 @@
 package game.climatar.systems.weather;
 
+
+
 public class WeatherSystemController {
 
+    private Integer BadPercipitation = 20;
+    private double BadTemperature = 50;
+
+
+    public boolean IsSafeTemperature(nation Nation){
+        if (GetTemperatureLevel(Nation)>BadTemperature){
+            return false;
+        }
+        else
+            return true;
+
+    }
+
+    public boolean IsDrought(nation Nation) {
+        if (GetPercipitationLevel(Nation) < BadPercipitation)
+            return true;
+
+        else
+            return false;
+    }
+
+    public Double GetTemperatureLevel(nation Nation){
+        return Nation.temperature;
+    }
+
+    public Integer GetPercipitationLevel(nation Nation){
+        return Nation.Percipitation;
+    }
 }
