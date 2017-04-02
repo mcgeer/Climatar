b8385078c4cd5c1acfbc9e60df0db1e4b95968d1
Riley McGee <mcgeer@mcmaster.ca>
2017-04-02 17:10:37 -0400

    Model of GHG Emissions

--- src/core/src/game/climatar/systems/ghg/GHGSystemModel.java
+++ src/core/src/game/climatar/systems/ghg/GHGSystemModel.java
@@ -1,7 +1,40 @@
 package game.climatar.systems.ghg;
 
+/**
+ * Holds information on GHG Emissions for a Region
+ */
 public class GHGSystemModel {
+    //GHG Emissions by given country in
+    private int GHGEmissionsPerUpdate;
 
-	
-	
+    /**
+     * Generate a new GHGSystemModel with an initial emission level of initialEmissions
+     */
+    public GHGSystemModel(int initialEmissions){
+        GHGEmissionsPerUpdate = initialEmissions;
+    }
+
+    /**
+     * Returns the current level of GHG being added per update
+     * @return Current level of emissions for the region
+     */
+    public int getGHGEmissionsPerUpdate(){
+        return GHGEmissionsPerUpdate;
+    }
+
+    /**
+     * Apply a change to emissions per update
+     * @param deltaGHGEmissions Change being applied to the emissions per update
+     */
+    public void updateGHGPerUpdate(int deltaGHGEmissions){
+        GHGEmissionsPerUpdate += deltaGHGEmissions;
+    }
+
+    /**
+     * Update implies that a phase has passed, update world state above
+     * @return How much GHG emissions are being contributed by this phase
+     */
+    public int Update(){
+        return GHGEmissionsPerUpdate;
+    }
 }
