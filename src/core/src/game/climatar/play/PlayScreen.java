a07b0a613bb47f0fd63004f36055dda8c74bec53
Wenbin Yuan <leoyuanwb@gmail.com>
2017-04-03 01:03:40 -0400

    Playscreen basic structure updated

--- src/core/src/game/climatar/play/PlayScreen.java
+++ src/core/src/game/climatar/play/PlayScreen.java
@@ -1,5 +1,46 @@
 package game.climatar.play;
 
-public class PlayScreen {
+import com.badlogic.gdx.Screen;
 
+public class PlayScreen implements Screen {
+
+    public PlayScreen(PlayScreenController playScreenController, float hudScale) {
+        this.playScreenController = playScreenController;
+        this.hudScale = hudScale;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
