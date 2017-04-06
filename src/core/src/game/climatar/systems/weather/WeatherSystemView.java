29c68be7440c690e4c44427abf273e302785c35a
mcgeer <Rileypatrickmcgee@gmail.com>
2017-04-06 00:00:31 -0400

    Boilerplate View for James

--- src/core/src/game/climatar/systems/weather/WeatherSystemView.java
+++ src/core/src/game/climatar/systems/weather/WeatherSystemView.java
@@ -1,49 +1,31 @@
 package game.climatar.systems.weather;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Group;
 
-public class WeatherSystemView implements Screen {
+import game.climatar.architecture.Model;
+import game.climatar.architecture.View;
+
+public class WeatherSystemView extends View {
+
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
+	public void build(Group group) {
+
 	}
 
 	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+	public void layout(float x, float y, float width, float height) {
+
 	}
 
 	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void update(Model model) {
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
 
+	}
 }
